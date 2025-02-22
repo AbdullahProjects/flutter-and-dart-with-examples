a0a32e03c1f305cc9bb605d13f06d4624ba478a9
Abdullah Khan Kakar <2022bbit71@student.uet.edu.pk>
2025-02-22 16:31:38 +0500

    async vs isolate

--- async_vs_isolate_example/lib/main.dart
+++ async_vs_isolate_example/lib/main.dart
@@ -14,8 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
       home: const MyHomePage(),
@@ -23,75 +24,105 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final int totalValue = 2000000000;
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text(
-          "async vs isolate",
-          style: TextStyle(
-            color: Colors.white,
-          ),
+        title: const Text("Async vs Isolate",
+            style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.purpleAccent,
+        elevation: 5,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
         ),
-        backgroundColor: Colors.pink.withValues(alpha: 0.7),
       ),
-      body: SizedBox(
+      body: Container(
         width: size.width,
+        padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            const SizedBox(height: 20),
-            Lottie.asset(
-              "assets/lottie_animations/bouncing_ball.json",
-              width: size.width / 1.5,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(6),
-                ),
-                backgroundColor: Colors.pink.withValues(alpha: 0.7),
+            const SizedBox(height: 10),
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
               ),
-              onPressed: () async {
-                await asyncTask();
-              },
-              child: Text(
-                "Task with await",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 17,
+              elevation: 10,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Lottie.asset(
+                      "assets/lottie_animations/bouncing_ball.json",
+                      width: size.width / 2,
+                    ),
+                    const SizedBox(height: 30),
+                    const Text(
+                      "Choose a method to execute a heavy task:",
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 15),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        backgroundColor: Colors.blueAccent,
+                      ),
+                      onPressed: () async {
+                        await asyncTask();
+                        showToast(context, "Async task completed");
+                      },
+                      child: Text(
+                        "Task with await",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        backgroundColor: Colors.redAccent,
+                      ),
+                      onPressed: () async {
+                        ReceivePort receivePort = ReceivePort();
+                        await Isolate.spawn(isolatesTask, receivePort.sendPort);
+                        receivePort.listen((i) {
+                          print("Isolates task completed: $i");
+                          showToast(context, "Isolate task completed");
+                        });
+                      },
+                      child: Text(
+                        "Task with isolates",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            const SizedBox(height: 15),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(6),
-                ),
-                backgroundColor: Colors.pink.withValues(alpha: 0.7),
-              ),
-              onPressed: () async {
-                ReceivePort receivePort = ReceivePort();
-                await Isolate.spawn(isolatesTask, receivePort.sendPort);
-                receivePort.listen((i) {
-                  print("Isolates task completed: $i");
-                });
-              },
-              child: Text(
-                "Task with isolates",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 17,
-                ),
-              ),
-            ),
+            )
           ],
         ),
       ),
@@ -100,16 +131,29 @@ class MyHomePage extends StatelessWidget {
 
   asyncTask() async {
     int i = 0;
-    for (int x = i; x < 1000000000; x++) {
+    for (int x = i; x < totalValue; x++) {
       i = x++;
     }
     print("Async task completed: $i");
   }
+
+  void showToast(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        duration: Duration(seconds: 2),
+        behavior: SnackBarBehavior.floating,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        margin: EdgeInsets.all(20),
+      ),
+    );
+  }
 }
 
+// code outside the class for Isolate
 isolatesTask(SendPort sendPort) {
   int i = 0;
-  for (int x = i; x < 1000000000; x++) {
+  for (int x = i; x < 2000000000; x++) {
     i = x++;
   }
   sendPort.send(i);
